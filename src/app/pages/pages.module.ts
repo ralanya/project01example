9a5b5ef0f267cbe81aa98369d8c654bb8e8f8e0e
Richard Alanya <richard_ac@outlook.com>
2023-12-28 17:00:36 -0500

    feature/richard/2

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    FavoritesComponent
+    FavoritesComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
